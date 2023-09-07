c900a409a10e749ad2281224afa8c48cb6d00b28
guosheng <guosheng@mchz.com.cn>
2023-09-07 11:22:52 +0800

    feat: add antd5

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import Demo from './pages/demo'
 
 ReactDOM.render(
   <div>
     <div>Hello,World！</div>
+    <Demo />
   </div>,
   document.getElementById('root')
 );
